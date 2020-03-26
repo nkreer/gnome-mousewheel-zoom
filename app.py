7c397ddee54704f051c17f9da12f186f55460f2f
Niklas Kreer <kreergit@posteo.de>
2020-03-26 19:06:01 +0100

    Use pynput for mouse input without root
    fixes #1 as what is described is no longer valid for the project

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import mouse
+from pynput.mouse import Listener
 import os, sys
 
 # Perhaps some user configuration?
@@ -13,15 +13,10 @@ command_get = "gsettings get org.gnome.desktop.a11y.magnifier mag-factor"
 command_set = "gsettings set org.gnome.desktop.a11y.magnifier mag-factor"
 
 
-# Assemble a command to be run as the user even though we are root
-def assemble_user_command(command, user):
-    return 'sudo -u "' + user + '" dbus-launch --exit-with-session ' + command 
-
-
 # Return the magnification factor reported by gsettings
 def get_mag_factor():
     global executing_user
-    return float(os.popen(assemble_user_command(command_get, executing_user)).read())
+    return float(os.popen(command_get).read())
 
 
 global current_mag_factor
@@ -30,39 +25,39 @@ print(current_mag_factor)
 
 
 # Hook mouse events and react to scrolling
-def mouse_hook_listener(event):
+def scroll_hook_listener(x, y, dx, dy):
     global current_mag_factor
     global executing_user
 
-    # Only ever react to mousewheel changes
-    if isinstance(event, mouse.WheelEvent):
+    # Respond to the direction of movement accordingly
+    if dy > 0:
+        current_mag_factor += 0.1
+    elif dy < 0:
+        current_mag_factor -= 0.1
 
-        # Respond to the direction of movement accordingly
-        if event.delta > 0:
-            current_mag_factor += 0.1
-        elif event.delta < 0:
-            current_mag_factor -= 0.1
-
-        # Cannot possibly go lower than minimum
-        if current_mag_factor < min_zoom_factor:
-            current_mag_factor = min_zoom_factor
-            return
+    # Cannot possibly go lower than minimum
+    if current_mag_factor < min_zoom_factor:
+        current_mag_factor = min_zoom_factor
+        return
     
-        # Respect the maximum zoom factor
-        if current_mag_factor > max_zoom_factor:
-            current_mag_factor = max_zoom_factor
-            return
+    # Respect the maximum zoom factor
+    if current_mag_factor > max_zoom_factor:
+        current_mag_factor = max_zoom_factor
+        return
 
-        # GNOME limits magnification precision to two decimal points
-        current_mag_factor = round(current_mag_factor, 2)
+    # GNOME limits magnification precision to two decimal points
+    current_mag_factor = round(current_mag_factor, 2)
 
-        # Assemble the command to change the zoom factor & execute
-        final_change_command = assemble_user_command(command_set + " " + str(current_mag_factor), executing_user)
-        print(final_change_command)
-        print(os.popen(final_change_command).read())
+    # Assemble the command to change the zoom factor & execute
+    final_change_command = command_set + " " + str(current_mag_factor)
+    print(final_change_command)
+    print(os.popen(final_change_command).read())
 
 
-mouse.hook(mouse_hook_listener)
+# Register the listener with pynput
+with Listener(on_scroll=scroll_hook_listener) as listener:
+    print("listener activated")
+    listener.join()
 
 # Keep it running
 while True:
